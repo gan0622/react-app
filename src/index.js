6eedb38c36ca984e8e90dc140528f58ebe74ba64
jianyuan gan <jianyuangan@jianyuans-MacBook-Pro.local>
2020-12-29 22:22:39 -0600

    12.29

--- src/index.js
+++ src/index.js
@@ -5,11 +5,31 @@ import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css"
 import CoursesListComponent from "./components/CoursesListComponent";
 import reportWebVitals from './reportWebVitals';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import {BrowserRouter, Link, Route} from "react-router-dom";
+import CourseEditorComponent from "./components/CourseEditorComponent";
 
 ReactDOM.render(
-    <div className="container">
-      <CoursesListComponent student = "jianyuan"/>,
-    </div>,
+    <BrowserRouter>
+        <div className="container">
+            <Link to="/login">Login</Link> |
+            <Link to="/register">Register</Link> |
+            <Link to="/profile">Profile</Link> |
+            <Link to="/courses">Courses</Link> |
+            <Link to="/edit">Editor</Link>
+                <Route path="/login" exact component={Login}/>
+                <Route path="/register" exact component={Register}/>
+                <Route path="/profile" exact component={Profile}/>
+                <Route path="/courses" exact>
+                    <CoursesListComponent student="jianyuan"/>
+                </Route>
+                <Route path="/edit/:courseId"
+                       exact
+                       component={CourseEditorComponent}/>
+        </div>
+    </BrowserRouter>,
       document.getElementById('root')
 );
 
